44eb4c53ee993471fa4522e9a734e3ff2ec36d5f
JengunSeo <tjwjddns1302@ajou.ac.kr>
2026-09-10 16:35:25 +0900

    first ci/cd

--- src/main/java/com/example/devopsservice/AppContoller.java
+++ src/main/java/com/example/devopsservice/AppContoller.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class AppContoller {
     @GetMapping("/")
     public String index(){
-        return "hello world";
+        return "Good day";
     }
 }
